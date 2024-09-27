02dc0d540b0f6e8d03e08ea75bea5d25fe99c2ac
EhtishamSattar <ehtishamsattar888@gmail.com>
2024-09-27 11:53:04 +0500

    Initial Commit

---
+++ MessagingApp/MessagingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingAppApp.swift
+//  MessagingApp
+//
+//  Created by Mac on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
